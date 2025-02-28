32b0ecc96434242912494d7fc773f519a58420a9
Aghiles-S <aghiles.saghir@supdevinci-edu.fr>
2025-02-28 18:07:31 +0100

    Delete API key from code

--- test/test.py
+++ test/test.py
@@ -1,40 +1,59 @@
 import json
 import logging
-from idlelib.colorizer import color_config
+import os
+from datetime import datetime
 
 import httpx
-from black import color_diff, datetime
+from dotenv import load_dotenv
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)  # Logger creation
 
-# URL and headers definition
-url = "https://prim.iledefrance-mobilites.fr/marketplace/estimated-timetable"
-headers = {"apikey": "bl51pll3Cxab4dbEKmxU7qKGFbsKTAY7"}
+def main():
+    """
+    Main function
+    """
+    # Logger configuration
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - [test][%(levelname)s] - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="test.log",
+    )
+    logger = logging.getLogger(__name__)  # Logger creation
 
-try:
-    # Requête à l'API
-    response = httpx.get(url, headers=headers)
-    response.raise_for_status()  # Verify if the request was successful
+    # URL and headers definition
+    url = "https://prim.iledefrance-mobilites.fr/marketplace/estimated-timetable"
+    headers = {"apikey": ""}
+    # API key recuperation from .env file
+    load_dotenv()
+    headers["apikey"] = os.getenv("API_KEY")
 
-    # JSON response recuperation
-    data = response.json()
+    try:
+        # Requête à l'API
+        response = httpx.get(url, headers=headers)
+        response.raise_for_status()  # Verify if the request was successful
 
-    # Save data in a JSON file
-    with open(
-        f"data_{datetime.now().strftime('%Y%m%d%H%M%S')}.json", "w", encoding="utf-8"
-    ) as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+        # JSON response recuperation
+        data = response.json()
 
-    logger.info("Data saved in data.json.")
+        # Save data in a JSON file
+        with open(
+            f"data_{datetime.now().strftime('%Y%m%d%H%M%S')}.json",
+            "w",
+            encoding="utf-8",
+        ) as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
-except httpx.HTTPStatusError as e:
-    logger.error(f"HTTP Error {e.response.status_code}: {e}")
-except httpx.RequestError as e:
-    logger.error(f"Request Error: {e}")
-except json.JSONDecodeError:
-    logger.error("Error : JSON response could not be decoded.")
-except Exception as e:
-    logger.error(f"An error occurred: {e}")
+        logger.info("Data saved in data.json.")
+
+    except httpx.HTTPStatusError as e:
+        logger.error(f"HTTP Error {e.response.status_code}: {e}")
+    except httpx.RequestError as e:
+        logger.error(f"Request Error: {e}")
+    except json.JSONDecodeError:
+        logger.error("Error : JSON response could not be decoded.")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
